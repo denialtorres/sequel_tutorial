843bb86dc283895d4ce12fc334c1636027bbd496
Daniel Torres <denial.torres@gmail.com>
2022-02-07 15:10:24 -0600

    database bootstrap

---
+++ bootstrap.rb
@@ -0,0 +1,48 @@
+require "bundler"
+Bundler.require
+
+require "yaml"
+
+file_path = File.expand_path "../database.yaml", __FILE__
+file = YAML.load_file file_path
+
+DB = Sequel.connect(file)
+
+# Load validations for models
+Sequel::Model.plugin :validation_helpers
+
+DB.drop_table?(:order_items)
+DB.drop_table?(:orders)
+DB.drop_table?(:products)
+
+DB.create_table?(:products) do
+  primary_key :id
+  String :name
+  String :category
+  Float :price
+end
+
+[
+  %w(Apple Fruit),
+  %w(Veal Meat),
+  %w(Broccoli Vegetable),
+  %w(Tomato Fruit),
+  %w(Hammer Tool),
+  %w(Screwdriver Tool),
+  %w(Onion Vegetable)
+].each do |item|
+  title = item[0]
+  category = item[1]
+  DB[:products].insert name: title, category: category, price: rand(11.2...76.9)
+end
+
+DB.create_table?(:orders) do
+  primary_key :id
+end
+
+DB.create_table?(:order_items) do
+  primary_key :id
+  foreign_key :product_id, :products, null: false
+  foreign_key :order_id, :orders, null: false
+  Integer :quantity
+end
--- connect_with_yaml.rb
+++ connect_with_yaml.rb
@@ -6,8 +6,6 @@ require "yaml"
 file_path = File.expand_path "../database.yaml", __FILE__
 file = YAML.load_file file_path
 
-db = Sequel.connect(file)
+Sequel.connect(file)
 
 # db = Sequel.connect "sqlite://db.sqlite3"
-
-ap db[:posts]
